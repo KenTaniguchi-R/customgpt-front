d7787bd106c6caec7123f5cdd06d5ac2b5eb133c
RyuseiTaniguchi <taniguchi.ryusei@gmail.com>
2023-04-13 22:31:08 -0700

    Add temp login singup button on LP

--- src/toC/LP.jsx
+++ src/toC/LP.jsx
@@ -1,8 +1,25 @@
+import { Link } from 'react-router-dom';
+
 
 const LPtoC = () => {
 
   return (
-    <div>hello world</div>
+    <>
+      <div>hello world</div>
+      <Link to='/client/signup'>
+        <button>企業向け会員登録</button>
+      </Link>
+      <Link to='/client/login'>
+        <button>企業向けログイン</button>
+      </Link>
+      <br></br>
+      <Link to='/signup'>
+        <button>普通の会員登録</button>
+      </Link>
+      <Link to='/login'>
+        <button>普通のログイン</button>
+      </Link>
+    </>
   )
 }
 
