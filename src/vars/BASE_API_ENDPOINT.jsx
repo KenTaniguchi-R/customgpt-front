35b00836e13830f0f8a04b8ae2ed1fd27b7d840e
RyuseiTaniguchi <taniguchi.ryusei@gmail.com>
2023-04-13 22:48:06 -0700

    Adding dist

--- src/vars/BASE_API_ENDPOINT.jsx
+++ src/vars/BASE_API_ENDPOINT.jsx
@@ -1,10 +1,12 @@
-const DEBUG = false;
+// const DEBUG = false;
 
-let BASE_API_ENDPOINT = '';
-if (DEBUG) {
-  BASE_API_ENDPOINT = 'http://localhost:8000/';
-}else{
-  BASE_API_ENDPOINT = 'https://customgpt.herokuapp.com/';
-}
+// let BASE_API_ENDPOINT = '';
+// if (DEBUG) {
+//   BASE_API_ENDPOINT = 'http://localhost:8000/';
+// }else{
+//   BASE_API_ENDPOINT = 'https://customgpt.herokuapp.com/';
+// }
+
+const BASE_API_ENDPOINT = 'https://customgpt.herokuapp.com/';
 
 export default BASE_API_ENDPOINT;
