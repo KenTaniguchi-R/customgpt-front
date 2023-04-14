f6a8d4cbb14cf980191cdbfe1f827d2a289431ed
RyuseiTaniguchi <taniguchi.ryusei@gmail.com>
2023-04-13 22:45:40 -0700

    Add Debug vars

--- src/vars/BASE_API_ENDPOINT.jsx
+++ src/vars/BASE_API_ENDPOINT.jsx
@@ -1,3 +1,10 @@
-// const BASE_API_ENDPOINT = 'http://localhost:8000/';
-const BASE_API_ENDPOINT = 'https://customgpt.herokuapp.com/';
+const DEBUG = false;
+
+let BASE_API_ENDPOINT = '';
+if (DEBUG) {
+  BASE_API_ENDPOINT = 'http://localhost:8000/';
+}else{
+  BASE_API_ENDPOINT = 'https://customgpt.herokuapp.com/';
+}
+
 export default BASE_API_ENDPOINT;
