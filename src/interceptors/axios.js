40f359796d1239676fd7bbf1480e0a05e110e29d
RyuseiTaniguchi <taniguchi.ryusei@gmail.com>
2023-04-19 16:13:54 -0700

    Fix axios refresh

--- src/interceptors/axios.js
+++ src/interceptors/axios.js
@@ -4,6 +4,7 @@ import BASE_API_ENDPOINT from '../vars/BASE_API_ENDPOINT';
 
 let count = 0;
 
+// TODO: fix this
 axios.interceptors.response.use(
 
   // success
@@ -18,19 +19,29 @@ axios.interceptors.response.use(
     const originalRequest = error.config;
     const refresh = localStorage.getItem('refresh_token');
 
+
     // if access token expired
     if (error.response.status === 401) {
-      const res = await axios.post(`${BASE_API_ENDPOINT}api/token/refresh/`, {
-        refresh: refresh
-      })
-      if (res.status === 200) {
-        // update token
-        localStorage.setItem('access_token', res.data.access);
-        axios.defaults.headers.common['Authorization'] = 'JWT ' + res.data.access;
-        originalRequest.headers['Authorization'] = 'JWT ' + res.data.access;
-        return axios(originalRequest)
-      }else{
-        count += 1;
+      try{
+        const res = await axios.post(`${BASE_API_ENDPOINT}api/token/refresh/`, {
+          refresh: refresh
+        })
+
+        if (res.status === 200) {
+          // update token
+          localStorage.setItem('access_token', res.data.access);
+          axios.defaults.headers.common['Authorization'] = 'JWT ' + res.data.access;
+          originalRequest.headers['Authorization'] = 'JWT ' + res.data.access;
+          return axios(originalRequest)
+        }else{
+          count += 1;
+          localStorage.clear();
+          axios.defaults.headers.common['Authorization'] = null;
+          return Promise.reject(error)
+        }
+
+      }catch{
+        console.log('error')
         localStorage.clear();
         axios.defaults.headers.common['Authorization'] = null;
         return Promise.reject(error)
